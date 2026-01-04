6792d75b6c48a698d188e63c316f4512ac0a7cfd
Maksym Nosal <max74825@gmail.com>
2026-01-04 13:48:32 +0100

    feature: add windows .bat file

---
+++ educator/launch.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Don't forget to create 'token.txt' with your Pinggy API token before running this script.
+python -m venv venv
+call venv\Scripts\activate.bat
+pip install -r requirements.txt
+python educator.py
